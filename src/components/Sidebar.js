023a243913e41c5a5e174e165713926c5014f97f
W Rivera <wrivera@Ws-iMac.fios-router.home>
2024-12-21 20:21:01 -0500

    second

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,5 +1,10 @@
 import { useState, useEffect } from "react";
 import styles from './Sidebar.module.css';
+import Link from 'next/link';
+
+{/* <Link href="/form/ContactForm/">
+  <a>Contact Form</a>
+</Link> */}
 
 const Sidebar = () => {
   const [isMobile, setIsMobile] = useState(false);
@@ -22,13 +27,13 @@ const Sidebar = () => {
         {/* <li><a href="/form/newform">Form Creation_old</a></li>
         <li><a href="/form/Entries">Form Creation New</a></li> */}
          {/* <li><a href="/form/login">Login</a></li> */}
-        <li><a href="/form/ContactForm">ContactForm</a></li>
-        <li><a href="/form/fileupload">File Upload</a></li>
-        <li><a href="/form/Hidden">Multiselct</a></li>
-        <li><a href="/form/Pagelist">Pagelist</a></li>
-        <li><a href="/form/Postlist">PostList</a></li>
-        <li><a href="/Business_financing/financing">Business Financing</a></li>
-        <li><a href="/Stepper/Createstepper">MultistepForm</a></li>
+        <li><Link href="/form/ContactForm">ContactForm</Link></li>
+        <li><Link href="/form/fileupload">File Upload</Link></li>
+        <li><Link href="/form/Hidden">Multiselct</Link></li>
+        <li><Link href="/form/Pagelist">Pagelist</Link></li>
+        <li><Link href="/form/Postlist">PostList</Link></li>
+        <li><Link href="/Business_financing/financing"> Business Financing </Link></li>
+        <li><Link href="/Stepper/Createstepper"> MultistepForm </Link></li>
         {/* <li><a href="/form/SignatureForm">SignatureForm</a></li> */}
         {/* <li><a href="/form/fileupload">FileUpload</a></li> */}
       </ul>
